4ae8513665f550ebaef68f555eeea808eb2652dc
Alessandro Pianetta <alessandro.pianetta@gmail.com>
2022-11-19 18:39:51 -0800

    Adds loading component to upload page

--- src/routes/Root/root.index.jsx
+++ src/routes/Root/root.index.jsx
@@ -1,13 +1,14 @@
 import { Layout, Link, Logo } from "../../components";
 import "./root.styles.css";
 
-function onSubmit() {}
-
 export default function Root() {
   return (
     <Layout hidden>
       <div className="logo-container">
         <Logo lg />
+      </div>
+      <div className="image-container">
+        <img className="hero-img" src="../../Studying-pana.png" />
       </div>
       <div className="nav-container">
         <Link to="/upload" block text="Log In" />
--- src/routes/Upload/upload.index.jsx
+++ src/routes/Upload/upload.index.jsx
@@ -1,43 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "../../components";
-import { useState } from "react";
-import axios from 'axios'
+import axios from "axios";
 import "./upload.styles.css";
+import { TailSpin } from "react-loader-spinner";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const [file, setFile] = useState('')
+  const navigate = useNavigate();
+  const [file, setFile] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
 
   function handleChange(event) {
-    setFile(event.target.files[0])
+    setFile(event.target.files[0]);
   }
-  
+
   function handleSubmit(event) {
-    event.preventDefault()
-    const url = 'http://localhost:8000/generate_questions_pdf';
+    event.preventDefault();
+    const url = "http://localhost:8000/generate_questions_pdf";
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
     const config = {};
-    axios.post(url, formData, config).then((response) => {
-      console.log(response.data);
-    });
+    setLoading(true);
+    axios
+      .post(url, formData, config)
+      .then((response) => {
+        if (errorMsg) {
+          setErrorMsg("");
+        }
+        setLoading(false);
+        navigate("/results", response.data);
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.error(e);
+        setErrorMsg(e.message);
+        setLoading(false);
+      });
   }
 
   return (
     <Layout center>
-    <div className="App">
-      <img className="hero-img" src="../../No data-pana.png" />
-        <form onSubmit={handleSubmit}>
-          <h1>File Upload</h1>
-          <input type="file" onChange={handleChange}/>
-          <button type="submit">Upload</button>
-        </form>
-    </div>
+      {!loading ? (
+        <div className="App">
+          <img className="hero-img" src="../../No data-pana.png" />
+          <form onSubmit={handleSubmit}>
+            <h1>File Upload</h1>
+            <input type="file" onChange={handleChange} />
+            <button type="submit">Upload</button>
+          </form>
+        </div>
+      ) : (
+        <TailSpin
+          height="80"
+          width="80"
+          color="#94cae8"
+          ariaLabel="tail-spin-loading"
+          radius="1"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      )}
+      {errorMsg && (
+        <h3 style={{ color: "red" }}>{errorMsg}. Please try again.</h3>
+      )}
     </Layout>
   );
 }
 
 export default App;
-
-
-
-
